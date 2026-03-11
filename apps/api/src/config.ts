fecb6b64c49c68423f0fb32bd421a8a7b396e29b
Alec Winter <alec.winter@monday-consulting.com>
2026-03-11 17:16:07 +0800

    feat(api): update docker setup

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -34,5 +34,5 @@ export const CURRENCIES = [
 ] as const;
 
 export const UPDATE_INTERVAL_MS = 24 * 60 * 60 * 1000; // 24 hours
-export const API_PORT = Number(process.env.PORT) || 3000;
+export const API_PORT = Number(process.env.PORT) || 4001;
 export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
